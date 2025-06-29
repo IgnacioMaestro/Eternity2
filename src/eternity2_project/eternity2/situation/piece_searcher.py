b44072573cf45ddd7fdfe7bcddb18455fbac4bcc
Ignacio Maestro Girón <ignacio.maestro@tecnalia.com>
2025-06-29 02:02:44 +0200

    refactor: extract helper methods in `piece_searcher` for clarity and maintainability

--- src/eternity2_project/eternity2/situation/piece_searcher.py
+++ src/eternity2_project/eternity2/situation/piece_searcher.py
@@ -11,10 +11,15 @@ class PieceSearcher:
 
     def search(self) -> list[RotatedPiece]:
         rotated_pieces: list[RotatedPiece] = PieceSet().search(self.__constraints)
-        rotated_pieces_not_in_placed_pieces = []
-
-        for rotated_piece in rotated_pieces:
-            if rotated_piece.get_piece() not in [placed_piece.get_piece() for placed_piece in self.__placed_pieces]:
-                rotated_pieces_not_in_placed_pieces.append(rotated_piece)
-
+        rotated_pieces_not_in_placed_pieces: list[RotatedPiece] = self.remove_placed_pieces(rotated_pieces)
         return rotated_pieces_not_in_placed_pieces
+
+    def remove_placed_pieces(self, rotated_pieces: list[RotatedPiece]) -> list[RotatedPiece]:
+        rotated_pieces_not_in_placed_pieces = []
+        for rotated_piece in rotated_pieces:
+            if self.is_rotated_piece_not_in_placed_pieces(rotated_piece):
+                rotated_pieces_not_in_placed_pieces.append(rotated_piece)
+        return rotated_pieces_not_in_placed_pieces
+
+    def is_rotated_piece_not_in_placed_pieces(self, rotated_piece: RotatedPiece) -> bool:
+        return rotated_piece.get_piece() not in [placed_piece.get_piece() for placed_piece in self.__placed_pieces]
